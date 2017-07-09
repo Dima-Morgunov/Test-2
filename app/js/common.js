c5513c43a9bfe4196c1025c641f19deff7ab38fb
Dima <dima0morgunov@gmail.com>
2017-07-10 00:15:22 +0300

    Сделал основную часть

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,14 @@
 
+// выпадающее меню
+$(document).ready(function() {
+  $(".toggle-menu").click(function() {
+  $(this).toggleClass("on");
+  $(".main-menu").slideToggle();
+  return false;
+});
+});
+
+// параметры библиотеки слайдера
 $(document).ready(function(){
 $('.owl-carousel').owlCarousel({
     loop:true,
@@ -8,9 +18,9 @@ $('.owl-carousel').owlCarousel({
         0:{
             items:1,
             dots:false,
-            nav:true
+            nav:false
         },
-        600:{
+        768:{
             items:1,
             dots:false,
             nav:true
@@ -24,27 +34,27 @@ $('.owl-carousel').owlCarousel({
     }
 })
 });
+// подсвечивания в блока с заглавием при наведении на кнопку 
+price1.onmouseover = function(){price_tittle1.style.backgroundColor ='#49cbcd'};
+price1.onmouseout = function(){price_tittle1.style.backgroundColor =''};
 
+price2.onmouseover = function(){price_tittle2.style.backgroundColor ='#49cbcd'};
+price2.onmouseout = function(){price_tittle2.style.backgroundColor =''};
 
+price3.onmouseover = function(){price_tittle3.style.backgroundColor ='#49cbcd'};
+price3.onmouseout = function(){price_tittle3.style.backgroundColor =''};
 
+// фиксированое меню
+$(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+          $('.header-wrap').addClass('header-fixed');
+      } else {
+          $('.header-wrap').removeClass('header-fixed');
+      }
+});
 
-
-
-// initMap() - функция инициализации карты
-function initMap() {
-    // Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
-    var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
- 
-    // Обязательные опции с которыми будет проинициализированна карта
-    var mapOptions = {
-        center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
-        zoom: 8               // Зум по умолчанию. Возможные значения от 0 до 21
-    };
- 
-    // Создаем карту внутри элемента #map
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-};
-    $(document).ready(function(){
+// ajax запрос
+$(document).ready(function(){
         $('#btn_submit').click(function(){
             // собираем данные с формы
             var user_name    = $('#user_name').val();
